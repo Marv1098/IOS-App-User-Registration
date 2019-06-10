c75789e984deef6b5c0113c49745aec9e256f26f
Marvin <mvt5393@yahoo.com>
2019-06-10 17:52:55 -0400

    IOS App User Registration

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'myiosapp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for myiosapp
+  use_frameworks!
+  pod 'Alamofire', '~> 4.3'
+
+  target 'myiosappTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'myiosappUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- myiosapp/ViewController.swift
+++ myiosapp/ViewController.swift
@@ -6,10 +6,44 @@
 //  Copyright © 2019 Marvin Tailor. All rights reserved.
 //
 
+import Alamofire
 import UIKit
+
+let URL_USER_REGISTER = "https://192.168.0.6/myiosapp/v1/register.php"
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldUsername: UITextField!
+    @IBOutlet weak var textFieldPassword: UITextField!
+    @IBOutlet weak var textFieldEmail: UITextField!
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var textFieldPhone: UITextField!
+    @IBOutlet weak var labelMessage: UILabel!
+    
+    
+    @IBAction func buttonClick(_ sender: UIButton) {
+        let parameters: Parameters=[
+            "username":textFieldUsername.text!,
+            "password":textFieldPassword.text!,
+            "email":textFieldEmail.text!,
+            "name":textFieldName.text!,
+            "phone":textFieldPhone.text!
+        ]
+        
+        Alamofire.request(URL_USER_REGISTER, method: .post, parameters: parameters).responseJSON {
+            response in
+            
+            print(response)
+            
+            if let result = response.result.value {
+                
+                let jsonData = result as! NSDictionary
+                
+                self.labelMessage.text = jsonData.value(forKey: "message") as! String?
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
